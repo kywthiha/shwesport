20e9461dba52058184b4d1d0b2fafed2a61f7a86
Kyaw Thi Ha <kywthiha@github.com>
2020-02-08 20:50:52 +0630

    first commit

--- includes/connection.php
+++ includes/connection.php
@@ -15,10 +15,10 @@ if ($_SERVER['HTTP_HOST'] == "localhost" or $_SERVER['HTTP_HOST'] == "http://shw
 	//local live
 
 
-	DEFINE('DB_USER', 'baganhis_shwelivetv');
-	DEFINE('DB_PASSWORD', 'shwelivetv');
+	DEFINE('DB_USER', 'baganhis_shwemovies');
+	DEFINE('DB_PASSWORD', 'baganhis_shwemovies');
 	DEFINE('DB_HOST', 'baganhistory.com');
-	DEFINE('DB_NAME', 'baganhis_livetv');
+	DEFINE('DB_NAME', 'baganhis_shwemovies');
 }
 
 	
